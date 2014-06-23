5a9966d337804d8056854b03533b19cc9889d401
lubos krnac <lubos.krnac@gmail.com>
2014-06-23 07:24:04 +0100

    Coverage report generation

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var traceur = require('gulp-traceur');
 var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
 
 gulp.task('build', function () {
   return gulp.src('lib/jasstor.js')
@@ -10,7 +11,19 @@ gulp.task('build', function () {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('test', function(){
+gulp.task('test', ['build'], function(){
   return gulp.src('test/jasstorSpec.js')
     .pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('coverage', ['build'], function (cb) {
+  gulp.src(['dist/jasstor.js'])
+    .pipe(istanbul()) // Covering files
+    .on('finish', function () {
+      gulp.src(['test/jasstorSpec.js'])
+        .pipe(mocha())
+        // Creating the reports after tests runned
+        .pipe(istanbul.writeReports())
+        .on('end', cb);
+    });
 });
--- test/jasstorSpec.js
+++ test/jasstorSpec.js
@@ -1,7 +1,9 @@
 'use strict';
 
+var Jasstor = require('../dist/jasstor.js');
+
 describe('jasstor', function(){
   it('should store the password', function(){
-
+    var jasstor = new Jasstor('ggg');
   });
 });
