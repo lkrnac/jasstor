7018512dfb02af17be6af20bc59bed9f04147282
lubos krnac <lubos.krnac@gmail.com>
2014-09-01 22:06:04 +0100

    Promises based unit test for verification of password overwriting

--- test/jasstorSpec.js
+++ test/jasstorSpec.js
@@ -1,10 +1,14 @@
 'use strict';
 
-var fs = require('fs');
+//this include is only for debugging purposes
+//var traceur = require('traceur');
+
+var Bluebird = require('bluebird');
+var fs = Bluebird.promisifyAll(require('fs'));
 // jshint -W098
 var should = require('should');
 
-var Jasstor = require('../dist/jasstor.js');
+import Jasstor from '../dist/jasstor.js';
 
 var credentialsFile = 'testCredentials.txt';
 
@@ -12,6 +16,17 @@ var checkError = (err, done) => {
   if (err) {
     done(err);
   }
+};
+
+var readFilePromise = function (credentialsFile, userName) {
+  return fs.readFileAsync(credentialsFile)
+    .then(JSON.parse)
+    .then(jsonData => {
+      return jsonData[userName];
+    })
+    .catch((err) => {
+      throw err;
+    });
 };
 
 describe('jasstor', () => {
@@ -39,7 +54,7 @@ describe('jasstor', () => {
     beforeEach((done) => {
       jasstor.saveCredentials('user', 'password', (err) => done(err));
     });
-    it('should ovewrite the existing paword', done => {
+    it('should ovewrite the existing password', done => {
       fs.readFile(credentialsFile, (err, data) => {
         checkError(err, done);
         var jsonData = JSON.parse(data);
@@ -54,6 +69,29 @@ describe('jasstor', () => {
           });
         });
       });
+    });
+  });
+
+  describe('when creadentials file already exist using promises', () => {
+    var jasstorWithCallbacks = new Jasstor(credentialsFile);
+    var jasstor = Bluebird.promisifyAll(jasstorWithCallbacks);
+    beforeEach((done) => {
+      jasstor.saveCredentials('user', 'password', (err) => done(err));
+    });
+    it('should ovewrite the existing paword', done => {
+      var originalPassword = readFilePromise(credentialsFile, 'user');
+      var newPassword;
+      jasstor.saveCredentialsAsync('user', 'password1')
+        .then(() => {
+          newPassword = readFilePromise(credentialsFile, 'user');
+          newPassword.should.be.ok;
+          originalPassword.should.be.ok;
+          newPassword.should.not.equal(originalPassword);
+          done();
+        })
+        .catch((err) => {
+          throw err;
+        });
     });
   });
 
