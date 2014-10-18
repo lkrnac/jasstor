7be496050d60d77e9b641164f299e0157e3fbb68
lubos krnac <lubos.krnac@gmail.com>
2014-10-18 18:48:00 +0100

    Build process changed to enable watching without unpiping

--- gulpfile.js
+++ gulpfile.js
@@ -7,33 +7,68 @@ var istanbul = require('gulp-istanbul');
 var coveralls = require('gulp-coveralls');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var plumber = require('gulp-plumber');
+var lazypipe = require('lazypipe');
 
+var errorOccured = false;
 var paths = {
   scripts: './lib/jasstor.js',
   tests: ['./test/jasstorClbkSpec.js', './test/jasstorPromiseSpec.js'],
   dist: './dist/jasstor.js'
 };
 
+//Submit coverage report to overalls.io
+gulp.task('coveralls', ['test', 'checkError'], function () {
+  return gulp.src('./coverage/lcov.info').pipe(coveralls());
+});
+
+//This task is here to exit from process with error code
+//This way drone.io (CI server) knows that process failed
+//It is needed because gulp-plumber forces 0 error code even when error occurs.
+gulp.task('checkError', ['test'], function () {
+  if (errorOccured) {
+    console.log('Error occured, exitting build process... ');
+    process.exit(1);
+  }
+});
+
+var errorHandler = function () {
+  console.log('Error occured... ');
+  errorOccured = true;
+};
+
+var transpilePipe = lazypipe()
+  .pipe(plumber, {
+    errorHandler: errorHandler
+  })
+  .pipe(jshint)
+  .pipe(jshint.reporter, stylish)
+  .pipe(jshint.reporter, 'fail')
+  .pipe(traceur);
+
 //Compiles ES6 into ES5
 gulp.task('build', function () {
   return gulp.src(paths.scripts)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish))
-    .pipe(jshint.reporter('fail'))
-    .pipe(traceur())
+    .pipe(plumber({
+      errorHandler: errorHandler
+    }))
+    .pipe(transpilePipe())
     .pipe(gulp.dest('dist'));
 });
 
 //Transpile to ES5 and runs mocha test
 gulp.task('test', ['build'], function (cb) {
   gulp.src([paths.dist])
+    .pipe(plumber({
+      errorHandler: errorHandler
+    }))
     .pipe(istanbul())
     .on('finish', function () {
       gulp.src(paths.tests)
-        .pipe(jshint())
-        .pipe(jshint.reporter(stylish))
-        .pipe(jshint.reporter('fail'))
-        .pipe(traceur())
+        .pipe(plumber({
+          errorHandler: errorHandler
+        }))
+        .pipe(transpilePipe())
         .pipe(gulp.dest('tmp'))
         .pipe(mocha())
         .pipe(istanbul.writeReports())
@@ -41,15 +76,9 @@ gulp.task('test', ['build'], function (cb) {
     });
 });
 
-//Submit coverage report to overalls.io
-gulp.task('coveralls', ['test'], function () {
-  return gulp.src('./coverage/lcov.info').pipe(coveralls({
-    error: false
-  }));
-});
-
 gulp.task('watch', function () {
-  gulp.watch([paths.scripts, paths.tests], ['test']);
+  var filesToWatch = paths.tests.concat(paths.scripts);
+  gulp.watch(filesToWatch, ['test']);
 });
 
-gulp.task('default', ['test', 'coveralls']);
+gulp.task('default', ['test', 'checkError', 'coveralls']);
