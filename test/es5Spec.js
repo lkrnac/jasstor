c5476c7908a711b25fc0b2b75c4026d0c1859217
lubos krnac <lubos.krnac@gmail.com>
2014-10-21 22:42:58 +0100

    Fix ES5 test

--- test/es5Spec.js
+++ test/es5Spec.js
@@ -10,22 +10,24 @@ var checkError = require('./testUtils').checkError;
 var credentialsFile = 'testCredentials.txt';
 
 describe('Transpiled Jasstor library', function () {
-    var jasstor = new Jasstor(credentialsFile);
-    beforeEach(function (done) {
-      //ignore error when testing file didn't exist before deletion
-      fs.unlink(credentialsFile, function (err) {
-        jasstor.saveCredentials('user', 'password', function (err) {
-          done(err);
-        });
-      });
+  var jasstor = new Jasstor(credentialsFile);
+  beforeEach(function (done) {
+    //done callback is not passed directly
+    //because we want to ignore possible error
+    fs.unlink(credentialsFile, function () {
+      done();
     });
-    it('should work when included into ES5 module', function(done) {
+  });
+
+  it('should store encrypted password', function (done) {
+    jasstor.saveCredentials('user', 'password', 'role', function () {
       fs.readFile(credentialsFile, function (err, data) {
         checkError(err, done);
-        var jsonData = JSON.parse(data);
-        jsonData.user.should.be.ok;
-        jsonData.user.should.not.equal('password');
+        var jsonDataForUser = JSON.parse(data).user;
+        jsonDataForUser.password.should.be.ok;
+        jsonDataForUser.password.should.not.equal('password');
         done();
       });
     });
   });
+});
